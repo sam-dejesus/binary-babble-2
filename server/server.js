2c0ea4dc05578daacb5b03939640a3850dbee1c4
samuel Dejesus <samuel1de2jesus3@gmail.com>
2025-07-06 13:38:16 -0400

    connected the db and added graphql to query the db

--- server/server.js
+++ server/server.js
@@ -1,43 +1,49 @@
-const path = require("path")
-const express = require("express");
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
-const exphbs = require("express-handlebars");
-const hbs = exphbs.create({ helpers: require("./utils/helpers") });
+const express = require('express');
+require('dotenv').config();
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-
-
-app.use(session(sess));
- 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
- 
-app.use(express.static("public"));
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
-
-
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
 });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/'));
+})
+
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`✅ API server running on port ${PORT}!`);
+    console.log(`🚀 Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+}).catch((err) => {
+  console.error('❌ Failed to connect to database:', err);
+});
+
+  };
+  
+// Call the async function to start the server
+  startApolloServer();
